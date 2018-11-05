b001d28205de8e86b6ff1aec937b422e208f230a
raulf2012 <raulf2012@gmail.com>
2018-11-05 09:50:23 -0800

    Added read_magmoms from file method, added example file

--- 03_dft_calcs/model.py
+++ 03_dft_calcs/model.py
@@ -9,7 +9,8 @@ Author: Raul A. Flores
 import sys
 sys.path.insert(0, "./PythonPackages"); sys.path.insert(0, "./PythonModules")
 from ase_modules.ase_methods import (
-    read_atoms_from_file, set_QE_calc_params, set_init_mag_moms,
+    read_atoms_from_file, set_QE_calc_params,
+    set_init_mag_moms, read_magmoms_from_file,
     ionic_opt, an_pdos,
     an_bands, an_ads_vib, clean_up_dft)
 #__|
@@ -42,8 +43,10 @@ calc, espresso_params = set_QE_calc_params(
     atoms=None, params={},
     load_defaults=True, init_inst=False)
 
+magmoms = read_magmoms_from_file(file_name="magmom_init.in")
+
 set_init_mag_moms(atoms, espresso_params=espresso_params,
-    read_from_file=True, inc_val_magmoms=False)
+    read_from_file=True, inc_val_magmoms=False, magmoms=magmoms)
 #__|
 
 #| - Protocols
